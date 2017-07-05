79caff5db9a0006ed144fe9ca73128e71d51f163
Oleksandra korel Kalinina <korery@gmail.com>
2017-07-05 16:00:49 +0300

    chore(*): improve for more flexible testing

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 global.Promise = require('pinkie-promise');
 
 var spawn = require('child_process').spawn;
@@ -5,11 +7,12 @@ var concat = require('concat-stream');
 
 module.exports = function(cliPath, combo, timeout) {
   if (!timeout) {
-    timeout = 200;
+    timeout = 300;
   }
-  
-  var proc = spawn('node', [cliPath], { stdio: [null, null, null] });
+
+  var proc = spawn('node', [cliPath], { stdio: 'pipe' });
   proc.stdin.setEncoding('utf-8');
+  proc.stdout.setEncoding('utf-8');
 
   var loop = function(combo) {
     if (combo.length > 0) {
@@ -24,10 +27,15 @@ module.exports = function(cliPath, combo, timeout) {
 
   loop(combo);
 
-  return new Promise(function(resolve) {
-    proc.stdout.pipe(concat(function(result) {
-      resolve(result.toString());
-    }));
+  return new Promise(function(resolve, reject) {
+    const steps = [];
+    proc.stdout.on('data', function(line) {
+      steps.push(line);
+    })
+    proc.stdout.on('close', function() {
+      resolve(steps);
+    })
+    proc.stdout.on('error', reject);
   });
 };
 
